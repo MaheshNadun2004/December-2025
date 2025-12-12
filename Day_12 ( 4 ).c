1337499b6820826bda842055a9a36015dca491e2
Mahesh Nadun <mahesh.nandun2220@gmail.com>
2025-12-12 18:15:20 +0530

    Create Day_12 ( 4 ).c

---
+++ Day_12 ( 4 ).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int squreNumber(int a){
+	int sNum = a*a;
+	return sNum;
+}
+int main(){
+	int a = 7;
+	printf("square of a number is %d " , squreNumber(a));
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
